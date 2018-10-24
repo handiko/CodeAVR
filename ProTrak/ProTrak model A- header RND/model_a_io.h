6842d3df76827eb838c1726d10d401f729934c11
handiko <handikogesang@gmail.com>
2018-10-24 22:20:54 +0700

    Adding Protrak

---
+++ ProTrak/ProTrak model A- header RND/model_a_io.h
@@ -0,0 +1,14 @@
+
+
+#ifndef _MODEL_A_IO_INCLUDED_
+#define _MODEL_A_IO_INCLUDED_
+
+#include <mega8.h>
+
+#define AFOUT	PORTB.4
+#define PTT	PORTB.5
+#define MERAH	PORTD.6
+#define HIJAU	PORTD.7
+
+#endif
+
